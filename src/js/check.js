3bb6deed7ce1b3e10f226dcbcba374026ccf915f
Tangerine Cat <kaineer@gmail.com>
2016-09-23 15:15:33 +0500

    Fix eslint errors

--- src/js/check.js
+++ src/js/check.js
@@ -1,6 +1,10 @@
 // src/js/check.js
 
+'use strict';
+
 var getMessage = function(a, b) {
+  var i, s;
+
   if(typeof (a) === 'boolean') {
     if(a) {
       return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
@@ -10,20 +14,22 @@ var getMessage = function(a, b) {
   }
 
   if(typeof (a) === 'number') {
-    return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b*4) + ' атрибутов';
+    return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
   }
 
   if(b) {
-    for(var i = 0, s = 0; i < a.length; ++i) {
+    for(i = 0, s = 0; i < a.length; ++i) {
       s += a[i] * b[i];
     }
 
     return 'Общая площадь артефактов сжатия: ' + s + ' пикселей';
   } else {
-    for(var i = 0, s = 0; i < a.length; ++i) {
+    for(i = 0, s = 0; i < a.length; ++i) {
       s += a[i];
     }
 
     return 'Количество красных точек во всех строчках изображения: ' + s;
   }
-}
+};
+
+getMessage();
